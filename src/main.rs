7074b49daa7c0bd5fbeebf96fd10d3a0ae87a85b
Kauã Klassmann <kaua.klassmann661@gmail.com>
2024-12-26 22:08:28 -0300

    added .env port

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,16 @@ mod routes;
 
 #[tokio::main]
 async fn main() {
-    let listener: TcpListener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let app_port: u16 = dotenvy::var("APP_PORT")
+        .ok()
+        .and_then(|p: String| p.parse().ok())
+        .unwrap_or(3000);
+
+    let address: String = format!("0.0.0.0:{}", app_port);
+
+    let listener: TcpListener = TcpListener::bind(address).await.unwrap();
+    
+    println!("\nServer running in port {}", app_port);
 
     axum::serve(listener, configure_routes()).await.unwrap()
 }
