3d15bbfed4d643e192ded9c6ad62030ad473e431
Kauã Klassmann <kaua.klassmann661@gmail.com>
2024-12-26 22:34:59 -0300

    cors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use routes::configure_routes;
 use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
 
 mod handlers;
 mod routes;
 
 #[tokio::main]
 async fn main() {
+    let cors: CorsLayer = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
     let app_port: u16 = dotenvy::var("APP_PORT")
         .ok()
         .and_then(|p: String| p.parse().ok())
@@ -14,8 +20,11 @@ async fn main() {
     let address: String = format!("0.0.0.0:{}", app_port);
 
     let listener: TcpListener = TcpListener::bind(address).await.unwrap();
-    
+
+    let routes = configure_routes()
+        .layer(cors);
+
     println!("\nServer running in port {}", app_port);
 
-    axum::serve(listener, configure_routes()).await.unwrap()
+    axum::serve(listener, routes).await.unwrap()
 }
