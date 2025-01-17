458cd073cbc11bf9c9df82e9e51482135c73d7ec
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-01-17 20:23:12 -0300

    added dotenv of cors configure

--- src/config/cors.rs
+++ src/config/cors.rs
@@ -1,10 +1,21 @@
-use std::sync::LazyLock;
+use std::{env, sync::LazyLock};
 
+use axum::http::HeaderValue;
+use dotenvy::dotenv;
 use tower_http::cors::{Any, CorsLayer};
 
-pub static CORS: LazyLock<CorsLayer> = LazyLock::new(|| {
+pub static CORS: LazyLock<CorsLayer> = LazyLock::new(configure_cors);
+
+fn configure_cors() -> CorsLayer {
+    dotenv().ok();
+
+    let origin = env::var("CORS_ORIGIN")
+        .expect("CORS_ORIGIN not found on .env file")
+        .parse::<HeaderValue>()
+        .unwrap();
+
     CorsLayer::new()
-        .allow_origin(Any)
+        .allow_origin(origin)
         .allow_methods(Any)
         .allow_headers(Any)
-});
+}
