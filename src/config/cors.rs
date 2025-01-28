6dd5d75af5ea35987a4dc9a7b011aa5a54ad0df6
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-01-28 15:55:00 -0300

    config cors has been improved

--- src/config/cors.rs
+++ src/config/cors.rs
@@ -1,16 +1,13 @@
 use std::{env, sync::LazyLock};
 
 use axum::http::HeaderValue;
-use dotenvy::dotenv;
 use tower_http::cors::{Any, CorsLayer};
 
 pub static CORS: LazyLock<CorsLayer> = LazyLock::new(configure_cors);
 
 fn configure_cors() -> CorsLayer {
-    dotenv().ok();
-
     let origin = env::var("CORS_ORIGIN")
-        .expect("CORS_ORIGIN not found on .env file")
+        .expect("CORS_ORIGIN not found at .env file")
         .parse::<HeaderValue>()
         .unwrap();
 
