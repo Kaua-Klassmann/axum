a25d97e766518835a5d6d646ff8d861a62cb4c78
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-02-01 21:49:19 -0300

    Added test database for test

--- src/config/database.rs
+++ src/config/database.rs
@@ -3,7 +3,17 @@ use std::env;
 use sea_orm::ConnectOptions;
 
 pub fn get_database_options() -> ConnectOptions {
-    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not found at .env file");
+    let db_url: String;
+    let db_schema: String;
+    
+    if cfg!(test) {
+        db_url = env::var("TEST_DATABASE_URL").expect("TEST_DATABASE_URL not found at .env file");
+        db_schema = env::var("TEST_DATABASE_SCHEMA").expect("TEST_DATABASE_SCHEMA not found at .env file");
+    } else {
+        db_url = env::var("DATABASE_URL").expect("DATABASE_URL not found at .env file");
+        db_schema = env::var("DATABASE_SCHEMA").expect("DATABASE_SCHEMA not found at .env file");
+    }
+
     let db_max_connections = env::var("DATABASE_MAX_CONNECTIONS")
         .expect("DATABASE_MAX_CONNECTIONS not found at .env file")
         .parse::<u32>()
@@ -12,7 +22,6 @@ pub fn get_database_options() -> ConnectOptions {
         .expect("DATABASE_MIN_CONNECTIONS not found at .env file")
         .parse::<u32>()
         .unwrap();
-    let db_schema = env::var("DATABASE_SCHEMA").expect("DATABASE_SCHEMA not found at .env file");
 
     ConnectOptions::new(db_url)
         .max_connections(db_max_connections)
