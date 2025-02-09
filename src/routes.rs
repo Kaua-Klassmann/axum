517200a973a98fe61df685919a82365f7b49e2d8
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-02-09 15:36:55 -0300

    The handlers has been the names changed

--- src/routes.rs
+++ src/routes.rs
@@ -3,13 +3,13 @@ use axum::{
     routing::{get, post}
 };
 
-use crate::{handlers::*, state::AppState};
+use crate::{handlers, state::AppState};
 
 pub fn configure_routes() -> Router<AppState> {
     Router::new()
         .route("/", get(|| async { "Hello World!" }))
-        .route("/path/{name}", post(path_handlers::path))
-        .route("/query", post(query_handlers::query))
-        .route("/json", post(json_handlers::json))
-        .route("/create_user", post(user_handlers::create_user))
+        .route("/path/{name}", post(handlers::path::path))
+        .route("/query", post(handlers::query::query))
+        .route("/json", post(handlers::json::json))
+        .route("/create_user", post(handlers::user::create_user))
 }
